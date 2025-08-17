dddc313c8d1b6ac258102180b3ac982b4a6367f6
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2025-08-17 17:26:32 -0300

    update: inserindo Image ao inves de background image deixando a responsividade melhor

--- src/components/featCards/index.tsx
+++ src/components/featCards/index.tsx
@@ -1,16 +1,20 @@
+import Image from "next/image";
 import "./featCard.css";
 
 type featCardsProps = {
     backImg: string;
     featTitle: string;
     featText: string;
+    descImg: string;
 }
 
-export default function FeatCards({ backImg,featTitle, featText }: featCardsProps) {
+export default function FeatCards({ backImg,featTitle, featText,descImg }: featCardsProps) {
   return (
     <div className="features-grid">
       <div className="feature-card">
-        <div className="feature-img" style={{ backgroundImage: `url(${backImg})` }}></div>
+        <div className="feature-img" >
+          <Image className="featImg" width={100} height={200} alt={`${descImg}`} src={`${backImg}`}></Image>
+        </div>
         <div className="feature-text">
           <h3>{featTitle}</h3>
           <p>
